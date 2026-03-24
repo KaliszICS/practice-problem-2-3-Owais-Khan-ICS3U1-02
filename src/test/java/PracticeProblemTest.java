08c54f118113dde2e9a0aa143227dc0e1d0cfa99
Owais-Khan-ICS3U1-02 <343318663@tdsb.ca>
2026-03-24 08:02:18 -0400

    Update PracticeProblemTest.java

--- src/test/java/PracticeProblemTest.java
+++ src/test/java/PracticeProblemTest.java
@@ -8,4 +8,24 @@ public class PracticeProblemTest {
       assertEquals(0, PracticeProblem.circleArea(0));
    }
 
+   @Test
+   void testExample() {
+      assertEquals(3.141592653589793, PracticeProblem.circleArea(1.0));
+   }
+
+   @Test
+   void testExample() {
+      assertEquals(78.53981633974483, PracticeProblem.circleArea(5.0));
+   }
+
+   @Test
+   void testExample() {
+      assertEquals(3489.960407194446, PracticeProblem.circleArea(33.33));
+   }
+
+   @Test
+   void testExample() {
+      assertEquals(31.006276680299816, PracticeProblem.circleArea(3.141592653589793));
+   }
+
 }
