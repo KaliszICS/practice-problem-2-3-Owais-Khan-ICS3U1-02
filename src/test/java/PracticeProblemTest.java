3a6f872306f913c4e1a809c2b16d67f6ef42302a
Owais-Khan-ICS3U1-02 <343318663@tdsb.ca>
2026-03-24 08:59:46 -0400

    Update PracticeProblemTest.java

--- src/test/java/PracticeProblemTest.java
+++ src/test/java/PracticeProblemTest.java
@@ -3,29 +3,127 @@ import org.junit.jupiter.api.*;
 
 public class PracticeProblemTest {
 
+   // test 0
    @Test
-   void testZero() {
+   void test1_1() {
       assertEquals(0, PracticeProblem.circleArea(0));
    }
 
+   // test 1
    @Test
-   void testOne() {
+   void test1_2() {
       assertEquals(3.141592653589793, PracticeProblem.circleArea(1.0));
    }
 
+   // test simple double
    @Test
-   void testSimpleDouble() {
+   void test1_3() {
       assertEquals(78.53981633974483, PracticeProblem.circleArea(5.0));
    }
 
+   // test larger double
    @Test
-   void testLargerDouble() {
+   void test1_4() {
       assertEquals(3489.960407194446, PracticeProblem.circleArea(33.33));
    }
 
+   // test long double
    @Test
-   void testLongDouble() {
+   void test1_5() {
       assertEquals(31.006276680299816, PracticeProblem.circleArea(3.141592653589793));
    }
 
+   //minutes to hours (int)
+
+   // test 0
+   @Test
+   void test2_1() {
+      assertEquals("0 hours and 0 minutes", PracticeProblem.minutesToHours(0));
+   }
+
+   @Test
+   void test2_2() {
+      assertEquals("0 hours and 0 minutes", PracticeProblem.minutesToHours(0));
+   }
+
+   @Test
+   void test2_3() {
+      assertEquals(78.53981633974483, PracticeProblem.circleArea(5.0));
+   }
+
+   // test larger double
+   @Test
+   void test2_4() {
+      assertEquals(3489.960407194446, PracticeProblem.circleArea(33.33));
+   }
+
+   // test long double
+   @Test
+   void test2_5() {
+      assertEquals(31.006276680299816, PracticeProblem.circleArea(3.141592653589793));
+   }
+
+   //q3
+   
+   // test 0
+   @Test
+   void test3_1() {
+      assertEquals(0, PracticeProblem.circleArea(0));
+   }
+
+   // test 1
+   @Test
+   void test3_2() {
+      assertEquals(3.141592653589793, PracticeProblem.circleArea(1.0));
+   }
+
+   // test simple double
+   @Test
+   void test3_3() {
+      assertEquals(78.53981633974483, PracticeProblem.circleArea(5.0));
+   }
+
+   // test larger double
+   @Test
+   void test3_4() {
+      assertEquals(3489.960407194446, PracticeProblem.circleArea(33.33));
+   }
+
+   // test long double
+   @Test
+   void test3_5() {
+      assertEquals(31.006276680299816, PracticeProblem.circleArea(3.141592653589793));
+   }
+
+   //q4
+   
+   // test 0
+   @Test
+   void test4_1() {
+      assertEquals(0, PracticeProblem.circleArea(0));
+   }
+
+   // test 1
+   @Test
+   void test4_2() {
+      assertEquals(3.141592653589793, PracticeProblem.circleArea(1.0));
+   }
+
+   // test simple double
+   @Test
+   void test4_3() {
+      assertEquals(78.53981633974483, PracticeProblem.circleArea(5.0));
+   }
+
+   // test larger double
+   @Test
+   void test4_4() {
+      assertEquals(3489.960407194446, PracticeProblem.circleArea(33.33));
+   }
+
+   // test long double
+   @Test
+   void test4_5() {
+      assertEquals(31.006276680299816, PracticeProblem.circleArea(3.141592653589793));
+   }
 }
