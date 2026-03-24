f16dfdcb176a5cd97fb5d6766c327846420a8577
Owais-Khan-ICS3U1-02 <343318663@tdsb.ca>
2026-03-24 08:10:44 -0400

    Update PracticeProblemTest.java

--- src/test/java/PracticeProblemTest.java
+++ src/test/java/PracticeProblemTest.java
@@ -4,27 +4,27 @@ import org.junit.jupiter.api.*;
 public class PracticeProblemTest {
 
    @Test
-   void testExample() {
+   void testZero() {
       assertEquals(0, PracticeProblem.circleArea(0));
    }
 
    @Test
-   void testExample() {
+   void testOne() {
       assertEquals(3.141592653589793, PracticeProblem.circleArea(1.0));
    }
 
    @Test
-   void testExample() {
+   void testSimpleDouble() {
       assertEquals(78.53981633974483, PracticeProblem.circleArea(5.0));
    }
 
    @Test
-   void testExample() {
+   void testLargerDouble() {
       assertEquals(3489.960407194446, PracticeProblem.circleArea(33.33));
    }
 
    @Test
-   void testExample() {
+   void testLongDouble() {
       assertEquals(31.006276680299816, PracticeProblem.circleArea(3.141592653589793));
    }
 
